aa40f2e1a1d0614068086524fdc1767f972d67de
Colin Schmidt <75905471+colin-schmidt@users.noreply.github.com>
2020-12-15 21:26:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*btn.addEventListener ('click', function () {
+
+ }
+ );
+
+ separate div for buttons...how do I repeat the buttons 100x while keeping code dry?
+
+want to hide check or x depending on if task is done, so a (if task then hide) if statement
+
+maybe a hidden/pop-up modal window once all tasks are completed
+
+ */
